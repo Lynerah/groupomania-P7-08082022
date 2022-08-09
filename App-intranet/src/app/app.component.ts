367d078cd523e88fbaa132155ac61d8c80a66578
cdb <cdb@telemis.com>
2022-08-09 09:07:26 +0200

    ngFor

--- App-intranet/src/app/app.component.ts
+++ App-intranet/src/app/app.component.ts
@@ -7,32 +7,32 @@ import { IntranetPost } from './models/intranet-post.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  myPost: IntranetPost;
-  myOtherSnap: IntranetPost;
-  myLastSnap: IntranetPost;
+  intranetPosts: IntranetPost[];
 
 
   ngOnInit() {
-    this.myPost = {
-      title: 'Archibald',
-      description:'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps:0
-    };
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.intranetPosts = [
+      {
+        title: 'Archibald',
+        description:'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps:0
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }
