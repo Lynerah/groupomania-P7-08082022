4fcde8c866b301ace69ca205685a6c5bb2e728d6
cdb <cdb@telemis.com>
2022-08-16 10:29:48 +0200

    add router

--- App-intranet/src/app/app-routing.module.ts
+++ App-intranet/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntranetPostListComponent } from './intranet-post-list/intranet-post-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'intranetPosts' , component: IntranetPostListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
